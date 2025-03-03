efb0d4978a5bee7053baa764a6d906ee9d96d6e2
Fedor <fdor.peshkov@bk.ru>
2025-03-03 18:48:55 +0500

    ProjectAdd
    Добавление проекта в гит

---
+++ ProjectBV312/ProjectBV312/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace ProjectBV312
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NoteTextBox = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton4 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.NewNote = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenNote = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveNote = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveAsNote = new System.Windows.Forms.ToolStripMenuItem();
+            this.ClearNote = new System.Windows.Forms.ToolStripMenuItem();
+            this.HelpNote = new System.Windows.Forms.ToolStripMenuItem();
+            this.BoldBtn = new System.Windows.Forms.ToolStripButton();
+            this.ItalicBtn = new System.Windows.Forms.ToolStripButton();
+            this.UnderBtn = new System.Windows.Forms.ToolStripButton();
+            this.ClearBtn = new System.Windows.Forms.ToolStripButton();
+            this.FontBtn = new System.Windows.Forms.ToolStripButton();
+            this.ColorBtn = new System.Windows.Forms.ToolStripButton();
+            this.ImageBtn = new System.Windows.Forms.ToolStripButton();
+            this.SearchBtn = new System.Windows.Forms.ToolStripButton();
+            this.pdfViewer = new PdfiumViewer.PdfViewer();
+            this.searchTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NoteTextBox
+            // 
+            this.NoteTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NoteTextBox.Location = new System.Drawing.Point(0, 25);
+            this.NoteTextBox.Name = "NoteTextBox";
+            this.NoteTextBox.Size = new System.Drawing.Size(785, 386);
+            this.NoteTextBox.TabIndex = 0;
+            this.NoteTextBox.Text = "";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton4,
+            this.BoldBtn,
+            this.ItalicBtn,
+            this.UnderBtn,
+            this.ClearBtn,
+            this.FontBtn,
+            this.ColorBtn,
+            this.ImageBtn,
+            this.searchTextBox,
+            this.SearchBtn});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 2);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(785, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "ToolStrip";
+            // 
+            // toolStripDropDownButton4
+            // 
+            this.toolStripDropDownButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton4.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewNote,
+            this.OpenNote,
+            this.SaveNote,
+            this.SaveAsNote,
+            this.ClearNote,
+            this.HelpNote});
+            this.toolStripDropDownButton4.Image = global::ProjectBV312.Properties.Resources.saveas_5165;
+            this.toolStripDropDownButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton4.Name = "toolStripDropDownButton4";
+            this.toolStripDropDownButton4.Size = new System.Drawing.Size(29, 22);
+            this.toolStripDropDownButton4.Text = "File";
+            // 
+            // NewNote
+            // 
+            this.NewNote.Name = "NewNote";
+            this.NewNote.Size = new System.Drawing.Size(180, 22);
+            this.NewNote.Text = "New";
+            this.NewNote.Click += new System.EventHandler(this.NewNote_Click);
+            // 
+            // OpenNote
+            // 
+            this.OpenNote.Name = "OpenNote";
+            this.OpenNote.Size = new System.Drawing.Size(180, 22);
+            this.OpenNote.Text = "Open";
+            this.OpenNote.Click += new System.EventHandler(this.OpenNote_Click);
+            // 
+            // SaveNote
+            // 
+            this.SaveNote.Name = "SaveNote";
+            this.SaveNote.Size = new System.Drawing.Size(180, 22);
+            this.SaveNote.Text = "Save";
+            this.SaveNote.Click += new System.EventHandler(this.SaveNote_Click);
+            // 
+            // SaveAsNote
+            // 
+            this.SaveAsNote.Name = "SaveAsNote";
+            this.SaveAsNote.Size = new System.Drawing.Size(180, 22);
+            this.SaveAsNote.Text = "SaveAs";
+            this.SaveAsNote.Click += new System.EventHandler(this.SaveAsNote_Click);
+            // 
+            // ClearNote
+            // 
+            this.ClearNote.Name = "ClearNote";
+            this.ClearNote.Size = new System.Drawing.Size(180, 22);
+            this.ClearNote.Text = "Clear";
+            this.ClearNote.Click += new System.EventHandler(this.ClearNote_Click);
+            // 
+            // HelpNote
+            // 
+            this.HelpNote.Name = "HelpNote";
+            this.HelpNote.Size = new System.Drawing.Size(180, 22);
+            this.HelpNote.Text = "Help";
+            this.HelpNote.Click += new System.EventHandler(this.HelpNote_Click);
+            // 
+            // BoldBtn
+            // 
+            this.BoldBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.BoldBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BoldBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BoldBtn.Name = "BoldBtn";
+            this.BoldBtn.Size = new System.Drawing.Size(36, 22);
+            this.BoldBtn.Text = "Bold";
+            this.BoldBtn.Click += new System.EventHandler(this.BoldBtn_Click);
+            // 
+            // ItalicBtn
+            // 
+            this.ItalicBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ItalicBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ItalicBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ItalicBtn.Name = "ItalicBtn";
+            this.ItalicBtn.Size = new System.Drawing.Size(36, 22);
+            this.ItalicBtn.Text = "Italic";
+            this.ItalicBtn.Click += new System.EventHandler(this.ItalicBtn_Click);
+            // 
+            // UnderBtn
+            // 
+            this.UnderBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.UnderBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.UnderBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.UnderBtn.Name = "UnderBtn";
+            this.UnderBtn.Size = new System.Drawing.Size(62, 22);
+            this.UnderBtn.Text = "Underline";
+            this.UnderBtn.Click += new System.EventHandler(this.UnderBtn_Click);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ClearBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(38, 22);
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // FontBtn
+            // 
+            this.FontBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.FontBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.FontBtn.Name = "FontBtn";
+            this.FontBtn.Size = new System.Drawing.Size(35, 22);
+            this.FontBtn.Text = "Font";
+            this.FontBtn.Click += new System.EventHandler(this.FontBtn_Click);
+            // 
+            // ColorBtn
+            // 
+            this.ColorBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ColorBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ColorBtn.Name = "ColorBtn";
+            this.ColorBtn.Size = new System.Drawing.Size(40, 22);
+            this.ColorBtn.Text = "Color";
+            this.ColorBtn.Click += new System.EventHandler(this.ColorBtn_Click);
+            // 
+            // ImageBtn
+            // 
+            this.ImageBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ImageBtn.Image = global::ProjectBV312.Properties.Resources.add_image;
+            this.ImageBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ImageBtn.Name = "ImageBtn";
+            this.ImageBtn.Size = new System.Drawing.Size(23, 22);
+            this.ImageBtn.Text = "Image";
+            this.ImageBtn.Click += new System.EventHandler(this.ImageBtn_Click);
+            // 
+            // SearchBtn
+            // 
+            this.SearchBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.SearchBtn.Image = global::ProjectBV312.Properties.Resources.searchmagnifierinterfacesymbol1_79893__1_;
+            this.SearchBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SearchBtn.Name = "SearchBtn";
+            this.SearchBtn.Size = new System.Drawing.Size(23, 22);
+            this.SearchBtn.Text = "Search";
+            this.SearchBtn.Click += new System.EventHandler(this.SearchBtn_Click);
+            // 
+            // pdfViewer
+            // 
+            this.pdfViewer.Location = new System.Drawing.Point(0, 27);
+            this.pdfViewer.Name = "pdfViewer";
+            this.pdfViewer.Size = new System.Drawing.Size(785, 386);
+            this.pdfViewer.TabIndex = 2;
+            this.pdfViewer.Visible = false;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(100, 25);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.NoteTextBox);
+            this.Controls.Add(this.pdfViewer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Заметки";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox NoteTextBox;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton BoldBtn;
+        private System.Windows.Forms.ToolStripButton ItalicBtn;
+        private System.Windows.Forms.ToolStripButton UnderBtn;
+        private System.Windows.Forms.ToolStripButton ClearBtn;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton4;
+        private System.Windows.Forms.ToolStripMenuItem NewNote;
+        private System.Windows.Forms.ToolStripMenuItem OpenNote;
+        private System.Windows.Forms.ToolStripMenuItem SaveAsNote;
+        private System.Windows.Forms.ToolStripMenuItem HelpNote;
+        private System.Windows.Forms.ToolStripMenuItem ClearNote;
+        private System.Windows.Forms.ToolStripMenuItem SaveNote;
+        private System.Windows.Forms.ToolStripButton ImageBtn;
+        private System.Windows.Forms.ToolStripButton SearchBtn;
+        private PdfiumViewer.PdfViewer pdfViewer;
+        private System.Windows.Forms.ToolStripButton FontBtn;
+        private System.Windows.Forms.ToolStripButton ColorBtn;
+        private System.Windows.Forms.ToolStripTextBox searchTextBox;
+    }
+}
+
---
+++ ProjectBV312/ProjectBV312/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfiumViewer;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+
+namespace ProjectBV312
+{
+    public partial class Form1: Form
+    {
+        private string currentFilePath = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NewNote_Click(object sender, EventArgs e)
+        {
+            NoteTextBox.Clear();
+            currentFilePath = string.Empty;
+            this.Text = "Заметки - Новая заметка";
+        }
+
+        private void OpenNote_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Files (*.txt;*.rtf;*.pdf)|*.txt;*.rtf;*.pdf|Text Files (*.txt)|*.txt|RTF Files (*.rtf)|*.rtf|PDF Files (*.pdf)|*.pdf";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+
+                    if (filePath.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+                    {
+                        //Режим редактирования
+                        NoteTextBox.Visible = true;
+                        pdfViewer.Visible = false;
+                        NoteTextBox.LoadFile(filePath, RichTextBoxStreamType.PlainText);
+                    }
+                    else if (filePath.EndsWith(".rtf", StringComparison.OrdinalIgnoreCase))
+                    {
+                        NoteTextBox.Visible = true;
+                        pdfViewer.Visible = false;
+                        NoteTextBox.LoadFile(filePath, RichTextBoxStreamType.RichText);
+                    }
+                    else if (filePath.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase))
+                    {
+                            try
+                            {
+                                pdfViewer.Document?.Dispose(); // Освобождаем предыдущий документ
+                                pdfViewer.Document = PdfiumViewer.PdfDocument.Load(filePath);
+                                pdfViewer.Visible = true;
+                                NoteTextBox.Visible = false;
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Ошибка при открытии PDF: " + ex.Message);
+                            }
+                            /*try
+                            {
+                                //Загружаем PDF с помощью PdfiumViewer
+                                var document = PdfiumViewer.PdfDocument.Load(filePath);
+                                pdfViewer.Document = document;
+                                pdfViewer.Visible = true;
+                                NoteTextBox.Visible = false;
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Ошибка при открытии PDF: " + ex.Message);
+                            }*/
+                        }
+                }
+            }
+
+            /*OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text Files (*.txt)|*.txt|RTF files (*.rtf)|*.rtf|" +
+                "PDF files (*.pdf)|*.pdf|All Files (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = openFileDialog.FileName;
+                NoteTextBox.Text = File.ReadAllText(currentFilePath);
+                this.Text = $"Заметки - {Path.GetFileName(currentFilePath)}";
+            }*/
+        }
+
+        private void SaveNote_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currentFilePath))
+            {
+                File.WriteAllText(currentFilePath, NoteTextBox.Text);
+                this.Text = $"Заметки - {Path.GetFileName(currentFilePath)}";
+            }
+            else
+            {
+                MessageBox.Show("Вы не указали путь сохранения!", "!!!");
+            }
+        }
+
+        private void SaveAsNote_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Files (*.txt;*.rtf;*.pdf)|*.txt;*.rtf;*.pdf|Text Files (*.txt)|*.txt|RTF Files (*.rtf)|*.rtf|PDF Files (*.pdf)|*.pdf";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    if (filePath.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+                    {
+                        NoteTextBox.SaveFile(filePath, RichTextBoxStreamType.PlainText);
+                    }
+                    else if (filePath.EndsWith(".rtf", StringComparison.OrdinalIgnoreCase))
+                    {
+                        NoteTextBox.SaveFile(filePath, RichTextBoxStreamType.RichText);
+                    }
+                    else if (filePath.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase))
+                    {
+                        using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            Document pdfDoc = new Document(PageSize.A4);
+                            PdfWriter.GetInstance(pdfDoc, stream);
+                            pdfDoc.Open();
+
+                            using (Bitmap bitmap = new Bitmap(NoteTextBox.Width, NoteTextBox.Height))
+                            {
+                                NoteTextBox.DrawToBitmap(bitmap, new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height));
+
+                                using (MemoryStream imageStream = new MemoryStream())
+                                {
+                                    bitmap.Save(imageStream, System.Drawing.Imaging.ImageFormat.Png);
+                                    iTextSharp.text.Image pdfImage = iTextSharp.text.Image.GetInstance(imageStream.ToArray());
+
+                                    pdfImage.ScaleToFit(PageSize.A4.Width - 20, PageSize.A4.Height - 20);
+                                    pdfDoc.Add(pdfImage);
+                                }
+                            }
+
+                            pdfDoc.Close();
+                        }
+                    }
+                }
+            }
+
+            /*SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Text Files (*.txt)|*.txt|RTF files (*.rtf)|*.rtf|" +
+                "PDF files (*.pdf)|*.pdf|All Files (*.*)|*.*"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = saveFileDialog.FileName;
+            }*/
+        }
+
+        private void ClearNote_Click(object sender, EventArgs e)
+        {
+            NoteTextBox.Clear();
+        }
+
+        private void HelpNote_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Заметки.\n\n" +
+                            "1. Создайтеь новую заметку.\n" +
+                            "2. Открыть существующую заметку.\n" +
+                            "3. Сохранить изменения.\n" +
+                            "4. Очистить содержимое.\n",
+                            "Справка");
+        }
+
+        private void BoldBtn_Click(object sender, EventArgs e)
+        {
+            FontStyle style = NoteTextBox.SelectionFont.Style;
+            if (NoteTextBox.SelectionFont.Bold)
+            {
+                style = style & ~FontStyle.Bold;
+                BoldBtn.BackColor = Color.White;
+            }
+            else
+            {
+                style = style | FontStyle.Bold;
+                BoldBtn.BackColor = Color.LightGray;
+            }
+            NoteTextBox.SelectionFont = new System.Drawing.Font(NoteTextBox.SelectionFont, style);
+            NoteTextBox.Focus();
+        }
+
+        private void ItalicBtn_Click(object sender, EventArgs e)
+        {
+            FontStyle style = NoteTextBox.SelectionFont.Style;
+            if (NoteTextBox.SelectionFont.Italic)
+            {
+                style = style & ~FontStyle.Italic;
+                ItalicBtn.BackColor = Color.White;
+            }
+            else
+            {
+                style = style | FontStyle.Italic;
+                ItalicBtn.BackColor = Color.LightGray;
+            }
+            NoteTextBox.SelectionFont = new System.Drawing.Font(NoteTextBox.SelectionFont, style);
+            NoteTextBox.Focus();
+        }
+
+        private void UnderBtn_Click(object sender, EventArgs e)
+        {
+            FontStyle style = NoteTextBox.SelectionFont.Style;
+            if (NoteTextBox.SelectionFont.Underline)
+            {
+                style = style & ~FontStyle.Underline;
+                UnderBtn.BackColor = Color.White;
+            }
+            else
+            {
+                style = style | FontStyle.Underline;
+                UnderBtn.BackColor = Color.LightGray;
+            }
+            NoteTextBox.SelectionFont = new System.Drawing.Font(NoteTextBox.SelectionFont, style);
+            NoteTextBox.Focus();
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            NoteTextBox.Clear();
+            NoteTextBox.Font = new System.Drawing.Font(NoteTextBox.Font, FontStyle.Regular);
+
+            BoldBtn.BackColor = Color.White;
+            ItalicBtn.BackColor = Color.White;
+            UnderBtn.BackColor = Color.White;
+        }
+
+        private void ImageBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        string imagePath = openFileDialog.FileName;
+                        System.Drawing.Image img = System.Drawing.Image.FromFile(imagePath);
+                        Clipboard.SetImage(img);
+                        NoteTextBox.Paste();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ошибка при вставке изображения: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void FontBtn_Click(object sender, EventArgs e)
+        {
+            using (FontDialog fontDialog = new FontDialog())
+            {
+                if (fontDialog.ShowDialog() == DialogResult.OK)
+                {
+                    NoteTextBox.SelectionFont = fontDialog.Font;
+                }
+            }
+        }
+
+        private void ColorBtn_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    NoteTextBox.SelectionColor = colorDialog.Color;
+                }
+            }
+        }
+
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            string searchText = searchTextBox.Text;
+            int startIndex = 0;
+            NoteTextBox.SelectAll();
+            NoteTextBox.SelectionBackColor = NoteTextBox.BackColor;
+            while ((startIndex = NoteTextBox.Text.IndexOf(searchText, startIndex, StringComparison.OrdinalIgnoreCase)) != -1)
+            {
+                NoteTextBox.Select(startIndex, searchText.Length);
+                NoteTextBox.SelectionBackColor = Color.Yellow;
+                startIndex += searchText.Length;
+            }
+        }
+
+
+
+        //Как работать с этим pdf?
+    }
+}
