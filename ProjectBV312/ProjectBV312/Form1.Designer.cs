2ade169c5f78e878b7def063f5152ee631eb776a
makiru <foxyda4@gmail.com>
2025-03-05 20:49:22 +0500

    Marker is done...

--- ProjectBV312/ProjectBV312/Form1.Designer.cs
+++ ProjectBV312/ProjectBV312/Form1.Designer.cs
@@ -6,7 +6,7 @@
         /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
+        ///
         /// <summary>
         /// Освободить все используемые ресурсы.
         /// </summary>
