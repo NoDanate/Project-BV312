03c507283cc7c0923e36e8708901dddd8c18cc6f
Fedor <fdor.peshkov@bk.ru>
2025-03-05 21:28:53 +0500

    Увеличение кнопок интерфейса и замена split на drop down в  Маркерах

--- ProjectBV312/ProjectBV312/Form1.Designer.cs
+++ ProjectBV312/ProjectBV312/Form1.Designer.cs
@@ -48,9 +48,9 @@
             this.searchTextBox = new System.Windows.Forms.ToolStripTextBox();
             this.SearchBtn = new System.Windows.Forms.ToolStripButton();
             this.pdfViewer = new PdfiumViewer.PdfViewer();
-            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
-            this.BulletListBuutton = new System.Windows.Forms.ToolStripMenuItem();
-            this.NumberedListButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.MarkersBtn = new System.Windows.Forms.ToolStripDropDownButton();
+            this.NumberedBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.BulletBtn = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -71,12 +71,13 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.toolStrip1.AutoSize = false;
             this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripDropDownButton4,
             this.BoldBtn,
             this.ItalicBtn,
             this.UnderBtn,
-            this.toolStripSplitButton1,
+            this.MarkersBtn,
             this.ClearBtn,
             this.FontBtn,
             this.ColorBtn,
@@ -85,7 +86,7 @@
             this.SearchBtn});
             this.toolStrip1.Location = new System.Drawing.Point(0, 2);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(785, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(785, 50);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "ToolStrip";
             // 
@@ -102,78 +103,78 @@
             this.toolStripDropDownButton4.Image = global::ProjectBV312.Properties.Resources.saveas_5165;
             this.toolStripDropDownButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripDropDownButton4.Name = "toolStripDropDownButton4";
-            this.toolStripDropDownButton4.Size = new System.Drawing.Size(29, 22);
+            this.toolStripDropDownButton4.Size = new System.Drawing.Size(29, 47);
             this.toolStripDropDownButton4.Text = "File";
             // 
             // NewNote
             // 
             this.NewNote.Name = "NewNote";
-            this.NewNote.Size = new System.Drawing.Size(111, 22);
+            this.NewNote.Size = new System.Drawing.Size(180, 34);
             this.NewNote.Text = "New";
             this.NewNote.Click += new System.EventHandler(this.NewNote_Click);
             // 
             // OpenNote
             // 
             this.OpenNote.Name = "OpenNote";
-            this.OpenNote.Size = new System.Drawing.Size(111, 22);
+            this.OpenNote.Size = new System.Drawing.Size(180, 34);
             this.OpenNote.Text = "Open";
             this.OpenNote.Click += new System.EventHandler(this.OpenNote_Click);
             // 
             // SaveNote
             // 
             this.SaveNote.Name = "SaveNote";
-            this.SaveNote.Size = new System.Drawing.Size(111, 22);
+            this.SaveNote.Size = new System.Drawing.Size(180, 34);
             this.SaveNote.Text = "Save";
             this.SaveNote.Click += new System.EventHandler(this.SaveNote_Click);
             // 
             // SaveAsNote
             // 
             this.SaveAsNote.Name = "SaveAsNote";
-            this.SaveAsNote.Size = new System.Drawing.Size(111, 22);
+            this.SaveAsNote.Size = new System.Drawing.Size(180, 34);
             this.SaveAsNote.Text = "SaveAs";
             this.SaveAsNote.Click += new System.EventHandler(this.SaveAsNote_Click);
             // 
             // ClearNote
             // 
             this.ClearNote.Name = "ClearNote";
-            this.ClearNote.Size = new System.Drawing.Size(111, 22);
+            this.ClearNote.Size = new System.Drawing.Size(180, 34);
             this.ClearNote.Text = "Clear";
             this.ClearNote.Click += new System.EventHandler(this.ClearNote_Click);
             // 
             // HelpNote
             // 
             this.HelpNote.Name = "HelpNote";
-            this.HelpNote.Size = new System.Drawing.Size(111, 22);
+            this.HelpNote.Size = new System.Drawing.Size(180, 34);
             this.HelpNote.Text = "Help";
             this.HelpNote.Click += new System.EventHandler(this.HelpNote_Click);
             // 
             // BoldBtn
             // 
             this.BoldBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.BoldBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BoldBtn.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.BoldBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BoldBtn.Name = "BoldBtn";
-            this.BoldBtn.Size = new System.Drawing.Size(36, 22);
+            this.BoldBtn.Size = new System.Drawing.Size(62, 47);
             this.BoldBtn.Text = "Bold";
             this.BoldBtn.Click += new System.EventHandler(this.BoldBtn_Click);
             // 
             // ItalicBtn
             // 
             this.ItalicBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.ItalicBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ItalicBtn.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ItalicBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ItalicBtn.Name = "ItalicBtn";
-            this.ItalicBtn.Size = new System.Drawing.Size(36, 22);
+            this.ItalicBtn.Size = new System.Drawing.Size(60, 47);
             this.ItalicBtn.Text = "Italic";
             this.ItalicBtn.Click += new System.EventHandler(this.ItalicBtn_Click);
             // 
             // UnderBtn
             // 
             this.UnderBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.UnderBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.UnderBtn.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.UnderBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.UnderBtn.Name = "UnderBtn";
-            this.UnderBtn.Size = new System.Drawing.Size(62, 22);
+            this.UnderBtn.Size = new System.Drawing.Size(106, 47);
             this.UnderBtn.Text = "Underline";
             this.UnderBtn.Click += new System.EventHandler(this.UnderBtn_Click);
             // 
@@ -182,7 +183,7 @@
             this.ClearBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.ClearBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ClearBtn.Name = "ClearBtn";
-            this.ClearBtn.Size = new System.Drawing.Size(38, 22);
+            this.ClearBtn.Size = new System.Drawing.Size(64, 47);
             this.ClearBtn.Text = "Clear";
             this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
             // 
@@ -191,7 +192,7 @@
             this.FontBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.FontBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.FontBtn.Name = "FontBtn";
-            this.FontBtn.Size = new System.Drawing.Size(35, 22);
+            this.FontBtn.Size = new System.Drawing.Size(58, 47);
             this.FontBtn.Text = "Font";
             this.FontBtn.Click += new System.EventHandler(this.FontBtn_Click);
             // 
@@ -200,7 +201,7 @@
             this.ColorBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.ColorBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ColorBtn.Name = "ColorBtn";
-            this.ColorBtn.Size = new System.Drawing.Size(40, 22);
+            this.ColorBtn.Size = new System.Drawing.Size(66, 47);
             this.ColorBtn.Text = "Color";
             this.ColorBtn.Click += new System.EventHandler(this.ColorBtn_Click);
             // 
@@ -210,7 +211,7 @@
             this.ImageBtn.Image = global::ProjectBV312.Properties.Resources.add_image;
             this.ImageBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ImageBtn.Name = "ImageBtn";
-            this.ImageBtn.Size = new System.Drawing.Size(23, 22);
+            this.ImageBtn.Size = new System.Drawing.Size(23, 47);
             this.ImageBtn.Text = "Image";
             this.ImageBtn.Click += new System.EventHandler(this.ImageBtn_Click);
             // 
@@ -218,7 +219,7 @@
             // 
             this.searchTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.searchTextBox.Name = "searchTextBox";
-            this.searchTextBox.Size = new System.Drawing.Size(100, 25);
+            this.searchTextBox.Size = new System.Drawing.Size(100, 50);
             // 
             // SearchBtn
             // 
@@ -226,42 +227,45 @@
             this.SearchBtn.Image = global::ProjectBV312.Properties.Resources.searchmagnifierinterfacesymbol1_79893__1_;
             this.SearchBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.SearchBtn.Name = "SearchBtn";
-            this.SearchBtn.Size = new System.Drawing.Size(23, 22);
+            this.SearchBtn.Size = new System.Drawing.Size(23, 47);
             this.SearchBtn.Text = "Search";
             this.SearchBtn.Click += new System.EventHandler(this.SearchBtn_Click);
             // 
             // pdfViewer
             // 
+            this.pdfViewer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pdfViewer.Location = new System.Drawing.Point(0, 27);
             this.pdfViewer.Name = "pdfViewer";
             this.pdfViewer.Size = new System.Drawing.Size(785, 386);
             this.pdfViewer.TabIndex = 2;
             this.pdfViewer.Visible = false;
             // 
-            // toolStripSplitButton1
+            // MarkersBtn
             // 
-            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.BulletListBuutton,
-            this.NumberedListButton});
-            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
-            this.toolStripSplitButton1.Size = new System.Drawing.Size(65, 22);
-            this.toolStripSplitButton1.Text = "Markers";
+            this.MarkersBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.MarkersBtn.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BulletBtn,
+            this.NumberedBtn});
+            this.MarkersBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.MarkersBtn.Name = "MarkersBtn";
+            this.MarkersBtn.Size = new System.Drawing.Size(100, 47);
+            this.MarkersBtn.Text = "Markers";
             // 
-            // BulletListBuutton
+            // NumberedBtn
             // 
-            this.BulletListBuutton.Name = "BulletListBuutton";
-            this.BulletListBuutton.Size = new System.Drawing.Size(180, 22);
-            this.BulletListBuutton.Text = "Bullet";
-            this.BulletListBuutton.Click += new System.EventHandler(this.BulletListBuutton_Click);
+            this.NumberedBtn.Name = "NumberedBtn";
+            this.NumberedBtn.Size = new System.Drawing.Size(185, 34);
+            this.NumberedBtn.Text = "Numbered";
+            this.NumberedBtn.Click += new System.EventHandler(this.NumberedListButton_Click);
             // 
-            // NumberedListButton
+            // BulletBtn
             // 
-            this.NumberedListButton.Name = "NumberedListButton";
-            this.NumberedListButton.Size = new System.Drawing.Size(180, 22);
-            this.NumberedListButton.Text = "Numbered";
-            this.NumberedListButton.Click += new System.EventHandler(this.NumberedListButton_Click);
+            this.BulletBtn.Name = "BulletBtn";
+            this.BulletBtn.Size = new System.Drawing.Size(185, 34);
+            this.BulletBtn.Text = "Bullet";
+            this.BulletBtn.Click += new System.EventHandler(this.BulletListBuutton_Click);
             // 
             // Form1
             // 
@@ -301,9 +305,9 @@
         private System.Windows.Forms.ToolStripButton FontBtn;
         private System.Windows.Forms.ToolStripButton ColorBtn;
         private System.Windows.Forms.ToolStripTextBox searchTextBox;
-        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
-        private System.Windows.Forms.ToolStripMenuItem BulletListBuutton;
-        private System.Windows.Forms.ToolStripMenuItem NumberedListButton;
+        private System.Windows.Forms.ToolStripDropDownButton MarkersBtn;
+        private System.Windows.Forms.ToolStripMenuItem BulletBtn;
+        private System.Windows.Forms.ToolStripMenuItem NumberedBtn;
     }
 }
 
